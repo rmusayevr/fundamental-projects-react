f6976794e9ac74ffad820b8ea75f93636a97c026
rmusayevr <rmusayevr19@gmail.com>
2023-09-10 00:33:44 +0400

    cart

---
+++ cart-react/src/CartItem.jsx
@@ -0,0 +1,32 @@
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+const CartItem = ({ id, img, title, price, amount }) => {
+  const { removeItem, increaseAmount, decreaseAmount } = useGlobalContext();
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h5>{title}</h5>
+        <span className="item-price">${price}</span>
+        {/* remove button */}
+        <button className="remove-btn" onClick={() => removeItem(id)}>
+          remove
+        </button>
+      </div>
+      <div>
+        {/* increase amount */}
+        <button className="amount-btn" onClick={() => increaseAmount(id)}>
+          <FaChevronUp className="amount-icon" />
+        </button>
+        {/* amount */}
+        <span className="amount">{amount}</span>
+        {/* decrease amount */}
+        <button className="amount-btn" onClick={() => decreaseAmount(id)}>
+          <FaChevronDown className="amount-icon" />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
